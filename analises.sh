4ddb345acbc1ce0bdd9991ebe86e342922a25088
MariaLuiza-S <mluizasantos.18ml@gmail.com>
2024-03-01 14:17:09 -0300

    Maria
    atualização

--- analises.sh
+++ analises.sh
@@ -3,24 +3,38 @@
 for LIB in SRR16972309;
 do
 
-/home/imt/Desktop/Programas/sratoolkit.3.0.10-ubuntu64/bin/fastq-dump --split-files $LIB
+	#/home/imt/Desktop/Programas/sratoolkit.3.0.10-ubuntu64/bin/fastq-dump --split-files $LIB
 
-fastp -i fastq/${LIB}_1.fastq -I fastq/${LIB}_2.fastq -o fastq/${LIB}_1.trim.fastq -O fastq/${LIB}_2.trim.fastq #pré-processamento, avalia qualidade e realiza limpeza
+	#pré-processamento, avalia qualidade e realiza limpeza
+	fastp -i /home/imt/Desktop/leishmania/fastq/${LIB}_1.fastq \
+	-I /home/imt/Desktop/leishmania/fastq/${LIB}_2.fastq \
+	-o /home/imt/Desktop/leishmania/fastq/${LIB}_1.trim.fastq \
+	-O /home/imt/Desktop/leishmania/fastq/${LIB}_2.trim.fastq 
 
-bwa mem -t 4 ref/l_infantum/GCF_000002875.2_ASM287v2_genomic.fna fastq/${LIB}_1.trim.fastq fastq/${LIB}_2.trim.fastq | samtools sort --threads 4 -o map/${LIB}.bam #alinhamento contra o genoma de referência e posterior ordenamento
+	#alinhamento contra o genoma de referência e posterior ordenamento
+	bwa mem -t 4 \
+	/home/imt/Desktop/leishmania/ref/l_infantum/GCF_000002875.2_ASM287v2_genomic.fna \
+	/home/imt/Desktop/leishmania/fastq/${LIB}_1.trim.fastq \
+	/home/imt/Desktop/leishmania/fastq/${LIB}_2.trim.fastq | \
+	samtools sort --threads 4 \
+	-o /home/imt/Desktop/leishmania/map/${LIB}.bam 
+	
+	#cálculo de profundidade
+	samtools depth /home/imt/Desktop/leishmania/map/${LIB}.bam \
+	> /home/imt/Desktop/leishmania/depth/${LIB}.txt
 
-samtools depth map/${LIB}.bam > depth/${LIB}.txt #cálculo de profundidade
 
+	rm /home/imt/Desktop/leishmania/map/${LIB}.bam 
 
-rm map/${LIB}.bam 
+	Rscript /home/imt/Desktop/leishmania/calculo_somia.R /home/imt/Desktop/leishmania/depth/${LIB}.txt \
+	>/home/imt/Desktop/leishmania/somia/${LIB}_somia.txt
 
-Rscript /home/imt/Desktop/leishmania/calculo_somia.R /home/imt/Desktop/leishmania/depth/${LIB}.txt >/home/imt/Desktop/leishmania/somia/${LIB}_somia.txt
+echo "finalizado somia ${LIB}"
 
-echo "finalizado somia"
+	Rscript /home/imt/Desktop/leishmania/calculo_janela.R /home/imt/Desktop/leishmania/depth/${LIB}.txt \
+	>/home/imt/Desktop/leishmania/janela/${LIB}_janela.txt
 
-Rscript /home/imt/Desktop/leishmania/calculo_janela.R /home/imt/Desktop/leishmania/depth/${LIB}.txt >/home/imt/Desktop/leishmania/janela/${LIB}_janela.txt
-
-echo "finalizado janela"
+echo "finalizado janela ${LIB}"
 
 done
 
